6fcf8d3ca0cf1b9e2361e3f3cc696e23c5ec42bc
setianing-budi <setianingbudi2@gmail.com>
2022-06-07 22:24:15 +0700

    delete StoreResponse.kt

--- app/src/main/java/com/example/kuy_njajan/data/ApiInterface.kt
+++ app/src/main/java/com/example/kuy_njajan/data/ApiInterface.kt
@@ -1,11 +1,9 @@
 package com.example.kuy_njajan.data
 
 import com.example.kuy_njajan.model.ResponseModel
-import com.example.kuy_njajan.model.StoreResponse
 import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.POST
-import okhttp3.ResponseBody
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 
@@ -38,5 +36,5 @@ interface ApiInterface {
         @Field("nama_toko") namaToko: String,
         @Field("alamat_toko") alamatToko: String,
         @Field("no_telepon") noTelepon: String
-    ): Call<StoreResponse>
+    ): Call<ResponseModel>
 }
--- app/src/main/java/com/example/kuy_njajan/model/StoreResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kuy_njajan.model
-
-class StoreResponse {
-    var success = true
-    lateinit var message: String
-    var storeData: ArrayList<Store> = ArrayList()
-}
