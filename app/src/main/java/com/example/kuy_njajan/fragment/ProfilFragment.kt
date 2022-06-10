7b74304c81c19dbf1cf3a22cc71cc37599158445
DESKTOP-T5N6S9E <fitaas30@gmail.com>
2022-06-10 15:52:54 +0700

    Profile Shared preferen Login

--- app/src/main/java/com/example/kuy_njajan/fragment/ProfilFragment.kt
+++ app/src/main/java/com/example/kuy_njajan/fragment/ProfilFragment.kt
@@ -8,26 +8,40 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.RelativeLayout
+import android.widget.TextView
 import com.example.kuy_njajan.R
 import com.example.kuy_njajan.activity.shared.*
 import com.example.kuy_njajan.activity.ui.Login_Activity
 import com.example.kuy_njajan.activity.ui.RegisterToko
 import com.example.kuy_njajan.activity.ui.WelcomeSellerActivity
+import com.example.kuy_njajan.data.helper.Constant
+import com.example.kuy_njajan.data.helper.PrefHelper
 
 class ProfilFragment : Fragment() {
 
-    lateinit var s: SharedPreferencesLogin
+//    lateinit var s: SharedPreferencesLogin
+lateinit var prefHelper: PrefHelper
     lateinit var btnLogout: Button
     lateinit var btnMulaiJual: RelativeLayout
     lateinit var btnRiwayat : RelativeLayout
+    lateinit var  textUser : TextView
+    lateinit var  textNama : TextView
+    lateinit var  textTelp :TextView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater.inflate(R.layout.fragment_profil, container, false)
         init(view)
+        prefHelper = PrefHelper(requireActivity())
 
-        s = SharedPreferencesLogin(requireActivity())
+        textUser.text = prefHelper.getString( Constant.PREF_USERNAME )
+        textNama.text = prefHelper.getString( Constant.pref_nama )
+        textTelp.text = prefHelper.getString( Constant.pref_notelp )
+//        s = SharedPreferencesLogin(requireActivity())
         btnLogout.setOnClickListener {
-            s.setStatus(false)
-            startActivity(Intent(requireActivity(), Login_Activity::class.java))
+            prefHelper.clear()
+            moveIntent()
+//            s.setStatus(false)
+//            startActivity(Intent(requireActivity(), Login_Activity::class.java))
         }
 
         btnMulaiJual.setOnClickListener {
@@ -39,10 +53,17 @@ class ProfilFragment : Fragment() {
         return view
     }
 
+    private fun moveIntent(){
+        startActivity(Intent(requireActivity(), Login_Activity::class.java))
+
+    }
     private fun init(view: View) {
         btnLogout = view.findViewById(R.id.btn_logout)
         btnMulaiJual = view.findViewById(R.id.btn_mulaijual)
         btnRiwayat = view.findViewById(R.id.btn_riwayat)
+        textUser = view.findViewById(R.id.text_user)
+        textNama= view.findViewById(R.id.text_nama)
+        textTelp = view.findViewById(R.id.text_tlp)
     }
 
 }
